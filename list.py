d2cec878959bdf4f94f047c1ba82f76f2263e06c
Efimov-68 <r.efimov@list.ru>
2017-10-03 20:18:05 +0300

    py code test

---
+++ list.py
@@ -0,0 +1,11 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f']
+
+'''
+if 'a1' in letters:
+    print('Найдено "а" в списке letters')
+else:
+    print('Символ "а" не найден в списке')
+'''
+
+if 'a' in letters:
+    letters.remove('a')
