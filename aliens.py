5e43bdabddea01892c8c6313402d6d3e5d3e9317
Roman <r.efimov@list.ru>
2017-11-02 19:34:45 +0300

    py 7 ->>> 8

--- aliens.py
+++ aliens.py
@@ -4,12 +4,16 @@ def myFunction():
     aliens = {}
 
     aliens["name"] = input("Введите имя вашего инопланетянена:\n")
+    
     aliens["color"] = input("Введите цвет вашего " + aliens["name"] + "\n")
+    
     aliens["age"] = input("Сколько " + aliens["name"] + " лет?\n")
     aliens["age"] = int(aliens["age"])
+    
     aliens["planets"] = str(input("Откуда " + aliens["name"] + " прилетел?\n"))
     
-    print("\n", aliens, "\n")
+    aliens_list = list(aliens.items())
+    print("\n", aliens_list, "\n")
     
     aliens["var"] = input("Хотите попробовать еще?(y/n или д/н)\n")
 
