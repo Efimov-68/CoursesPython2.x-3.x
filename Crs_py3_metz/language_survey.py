06bbe50da586007bb155eb060df11cdf58e029e3
Roman <r.efimov@list.ru>
2017-11-09 21:32:13 +0300

    исправил критическую ошибку в файле  language_survey.py

--- Crs_py3_metz/language_survey.py
+++ Crs_py3_metz/language_survey.py
@@ -2,7 +2,6 @@ from survey import AnonymousSurvey
 
 question = "What language did you first learn to speak?"
 my_survey = AnonymousSurvey(question)
-
 my_survey.show_question()
 print("Enter 'q' at any time to quit.\n")
 while True:
